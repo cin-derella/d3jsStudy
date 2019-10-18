b00f9f0f469ca53ae07a55b25237b0eb94794b81
cin-derella <shirley_cinderella@outlook.com>
2019-10-18 21:30:18 +0800

    external data

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-
-
-const data = [
-    {width:400,height:500,fill:'yellow'},
-    {width:300,height:200,fill:'pink'},
-    {width:100,height:100,fill:'green'}
-  
-];
-
+//select svg container first
 const svg = d3.select('svg');
+d3.json('planets.json').then(data=>{
+    const circs = svg.selectAll('circle')
+        .data(data);
+    //add attrs to circs already in DOM
+    circs.attr('cy',200)
+        .attr('cx',d => d.distance)
+        .attr('r',d =>d.radius)
+        .attr('fill',d =>d.fill);
 
-// join  data to rects
-const rects = svg.selectAll('rect')
-    .data(data);
+    // append the enter selection to  the DOM
+    circs.enter()
+        .append('circle')
+            .attr('cy',200)
+            .attr('cx',d => d.distance)
+            .attr('r',d =>d.radius)
+            .attr('fill',d =>d.fill);
+})
 
-// add attrs to rects already in the DOM
-rects.attr('width',(d,i,n)=> d.width)
-    .attr('height',d =>d.height)
-    .attr('fill',d => d.fill);
 
-// append the enter selection to DOM
-rects.enter()
-    .append('rect')
-    .attr('width',(d,i,n)=> d.width)
-    .attr('height',d =>d.height)
-    .attr('fill',d => d.fill);
+
+
+
+
+
+
+// const data = [
+//     {width:400,height:500,fill:'yellow'},
+//     {width:300,height:200,fill:'pink'},
+//     {width:100,height:100,fill:'green'}
+  
+// ];
+
+// const svg = d3.select('svg');
+
+// // join  data to rects
+// const rects = svg.selectAll('rect')
+//     .data(data);
+
+// // add attrs to rects already in the DOM
+// rects.attr('width',(d,i,n)=> d.width)
+//     .attr('height',d =>d.height)
+//     .attr('fill',d => d.fill);
+
+// // append the enter selection to DOM
+// rects.enter()
+//     .append('rect')
+//     .attr('width',(d,i,n)=> d.width)
+//     .attr('height',d =>d.height)
+//     .attr('fill',d => d.fill);
 
 
 
