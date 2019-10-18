dbbc10afd6b60597afa751be8b9ddb1041c9341e
cin-derella <shirley_cinderella@outlook.com>
2019-10-18 21:04:42 +0800

    enter selection

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ const data = [
 ];
 
 const svg = d3.select('svg');
-const rect = svg.selectAll('rect')
-    .data(data)
+
+// join  data to rects
+const rects = svg.selectAll('rect')
+    .data(data);
+
+// add attrs to rects already in the DOM
+rects.attr('width',(d,i,n)=> d.width)
+    .attr('height',d =>d.height)
+    .attr('fill',d => d.fill);
+
+// append the enter selection to DOM
+rects.enter()
+    .append('rect')
     .attr('width',(d,i,n)=> d.width)
     .attr('height',d =>d.height)
-    .attr('fill',d => d.fill)
-console.log(rect);
+    .attr('fill',d => d.fill);
 
 
 
