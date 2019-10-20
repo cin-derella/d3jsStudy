1aeca099dd2b1fb0d7da84e584e9d0f58b6fd7ed
cin-derella <shirley_cinderella@outlook.com>
2019-10-20 13:55:00 +0800

    update data on the fly

--- index.js
+++ index.js
@@ -78,19 +78,44 @@ const update = (data) => {
 
 }
 
-db.collection('dishes').get().then(res => {
-    var data = [];
-    console.log(res);
-    res.docs.forEach(doc => {
-        console.log(doc.data());
-        data.push(doc.data());
-    });
-    update(data);
 
-    d3.interval(()=>{
-        data.pop();
-        update(data);
-    },3000)
+var data = [];
+// get data from firestore
+db.collection('dishes').onSnapshot(res=>{
+    res.docChanges().forEach(change=>{
+        const doc = {...change.doc.data(),id:change.doc.id};
+        switch(change.type){
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item=>item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id !== doc.id);  
+                break;
+            default:
+                break;
+        }
+    });
+    update(data)
+})
+
+
+
+    // var data = [];
+    // console.log(res);
+    // res.docs.forEach(doc => {
+    //     console.log(doc.data());
+    //     data.push(doc.data());
+    // });
+    // update(data);
+
+    // d3.interval(()=>{
+    //     data.pop();
+    //     update(data);
+    // },3000)
 
 
 
@@ -112,7 +137,6 @@ db.collection('dishes').get().then(res => {
     //     .attr('height',d=>y(d.orders))
     //     .attr('fill','orange')
     //     .attr('x',(d,i)=>i *70);
-})
 
 
 
