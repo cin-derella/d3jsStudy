ff000d168c3d52b09be42290110266d84b2b0546
cin-derella <shirley_cinderella@outlook.com>
2019-10-20 12:30:30 +0800

    updating function

--- index.js
+++ index.js
@@ -1,78 +1,99 @@
 //select svg container first
 const svg = d3.select('.canvas')
     .append('svg')
-    .attr('width',600)
-    .attr('height',600);
+    .attr('width', 600)
+    .attr('height', 600);
 
 //create margins and dimension
-const margin = {top:20,right:20,bottom:100,left:100};
-const graphWidth = 600 - margin.left-margin.right;
-const graphHeight = 600-margin.top-margin.bottom;
+const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
-    .attr('width',graphWidth)
-    .attr('height',graphHeight)
-    .attr('transform',`translate(${margin.left},${margin.top})`);
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
 const xAxisGroup = graph.append('g')
-    .attr('transform',`translate(0,${graphHeight})`);
+    .attr('transform', `translate(0,${graphHeight})`);
 const yAxisGroup = graph.append('g');
 
-db.collection('dishes').get().then(res =>{
+//scales
+const y = d3.scaleLinear()
+    .range([graphHeight, 0]);
+
+const x = d3.scaleBand()
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+//create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3.axisLeft(y)
+    .ticks(4)
+    .tickFormat(d => d + ' orders');
+
+
+
+
+//update x axis text
+ xAxisGroup.selectAll('text')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+    .attr('fill', 'orange');
+
+
+//update function
+const update = (data) => {
+    //updating domain scales
+        y.domain([0, d3.max(data, d => d.orders)]);
+        x.domain(data.map(item => item.name));
+        
+    //join the data to rect
+        const rects = graph.selectAll('rects')
+        .data(data);
+    
+    //remove exit selection
+    rects.exit().remove();
+
+    //update current shapes in DOM
+    rects.attr('width', x.bandwidth)
+        .attr('height', d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    //append the enter selection to the DOM
+    rects.enter()
+        .append('rect')
+        .attr('width', x.bandwidth)
+        .attr('height', d => graphHeight - y(d.orders))
+        .attr('fill', 'orange')
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    //call axes
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+}
+
+db.collection('dishes').get().then(res => {
     var data = [];
     console.log(res);
     res.docs.forEach(doc => {
         console.log(doc.data());
         data.push(doc.data());
-    })
+    });
 
-    console.log(data)
+    update(data);
 
-    const y = d3.scaleLinear()
-        .domain([0,1000])
-        .range([graphHeight,0]);
-     
+
+
     // const min = d3.min(data,d => d.orders);
     // const max = d3.max(data,d => d.orders);
     // const extent = d3.extent(data,d => d.orders);
 
-    const x = d3.scaleBand()
-        .domain(data.map(item =>item.name)) 
-        .range([0,500])
-        .paddingInner(0.2)
-        .paddingOuter(0.2);
-
-    //join the data to rect
-    const rects = graph.selectAll('rects')
-        .data(data);
-
-    rects.attr('width',x.bandwidth)
-        .attr('height',d=>graphHeight-y(d.orders))
-        .attr('fill','orange')
-        .attr('x',d=>x(d.name))
-        .attr('y',d=>y(d.orders));
-
-    rects.enter()
-        .append('rect')
-        .attr('width',x.bandwidth)
-        .attr('height',d=>graphHeight-y(d.orders))
-        .attr('fill','orange')
-        .attr('x',d=>x(d.name))
-        .attr('y',d=>y(d.orders));
-    
-    //create and all the axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y)
-        .ticks(4)
-        .tickFormat(d=>d+' orders');
-
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
-
-    xAxisGroup.selectAll('text')
-        .attr('transform','rotate(-40)')
-        .attr('text-anchor','end')
-        .attr('fill','orange');
 
     // rects.attr('width',50)
     //     .attr('height',d=>y(d.orders))
@@ -86,6 +107,8 @@ db.collection('dishes').get().then(res =>{
     //     .attr('fill','orange')
     //     .attr('x',(d,i)=>i *70);
 })
+
+
 
 
 
@@ -115,7 +138,7 @@ db.collection('dishes').get().then(res =>{
 //     {width:400,height:500,fill:'yellow'},
 //     {width:300,height:200,fill:'pink'},
 //     {width:100,height:100,fill:'green'}
-  
+
 // ];
 
 // const svg = d3.select('svg');
